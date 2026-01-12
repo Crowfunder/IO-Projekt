2697c96fbd30045fe08060e2fa520f413e018f67
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2026-01-12 20:42:48 +0100

    Oopsie

--- backend/components/camera_verification/faceid/__init__.py
+++ backend/components/camera_verification/faceid/__init__.py
@@ -2,7 +2,7 @@
 # FaceID
 Performs face scan verification, works by using the `face_recognition` module which uses a model that detects faces on images and produces their embeddings. The embeddings can then be easily compared to assess whether two faces match.
 
-## Key Components
+## Directory Structure
 
 #### `faceidService.py`
 - Contains the core logic for face recognition.
